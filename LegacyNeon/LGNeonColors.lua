a6151e1f758d3edb95dda155ac3da3b95861c0ca
Singharaj Usai <djjd22938@gmail.com>
2022-07-02 18:13:05 -0400

    legacy neon color

---
+++ LegacyNeon/LGNeonColors.lua
@@ -0,0 +1,566 @@
+function changecolors(Neon)
+	if Neon.Material == Enum.Material.Neon then
+		if Neon.Color == Color3.fromRGB(39, 70, 45) then -- Earth green
+			Neon.Color = Color3.fromRGB(81, 106, 86)
+		else
+			--print("")
+		end
+		if Neon.Color == Color3.fromRGB(80, 109, 84) then -- Slime green
+			Neon.Color = Color3.fromRGB(113, 131, 115)
+		else
+			--print("")
+		end
+		if Neon.Color == Color3.fromRGB(0, 143, 156) then -- Bright bluish green
+			Neon.Color = Color3.fromRGB(0, 174, 186)
+		else
+			--print("")
+		end
+		if Neon.Color == Color3.fromRGB(27, 42, 53) then -- Black
+			Neon.Color = Color3.fromRGB(66, 83, 94)
+		else
+			--print("")
+		end
+		if Neon.Color == Color3.fromRGB(33, 84, 185) then -- Deep blue
+			Neon.Color = Color3.fromRGB(76, 116, 175)
+		else
+			--print("")
+		end
+		if Neon.Color == Color3.fromRGB(0, 16, 176) then -- Dark blue
+			Neon.Color = Color3.fromRGB(37, 51, 176)
+		else
+			--print("")
+		end
+		if Neon.Color == Color3.fromRGB(0, 32, 96) then -- Navy blue
+			Neon.Color = Color3.fromRGB(40, 72, 122)
+		else
+			--print("")
+		end
+		if Neon.Color == Color3.fromRGB(44, 101, 29) then -- Parsley green
+			Neon.Color = Color3.fromRGB(85, 127, 70)
+		else
+			--print("")
+		end
+		if Neon.Color == Color3.fromRGB(40, 127, 71) then -- Dark green
+			Neon.Color = Color3.fromRGB(86, 144, 106)
+		else
+			--print("")
+		end
+		if Neon.Color == Color3.fromRGB(18, 238, 212) then -- Teal
+			Neon.Color = Color3.fromRGB(33, 235, 208)
+		else
+			--print("")
+		end
+		if Neon.Color == Color3.fromRGB(91, 93, 105) then -- Smokey grey
+			Neon.Color = Color3.fromRGB(121, 122, 129)
+		else
+			--print("")
+		end
+		if Neon.Color == Color3.fromRGB(82, 124, 174) then -- Steel blue
+			Neon.Color = Color3.fromRGB(115, 137, 163)
+		else
+			--print("")
+		end
+		if Neon.Color == Color3.fromRGB(51, 88, 130) then -- Storm blue
+			Neon.Color = Color3.fromRGB(91, 119, 151)
+		else
+			--print("")
+		end
+		if Neon.Color == Color3.fromRGB(16, 42, 220) then -- Lapis
+			Neon.Color = Color3.fromRGB(52, 83, 186)
+		else
+			--print("")
+		end
+		if Neon.Color == Color3.fromRGB(61, 21, 133) then -- Dark indigo
+			Neon.Color = Color3.fromRGB(99, 67, 228)
+		else
+			--print("")
+		end
+		if Neon.Color == Color3.fromRGB(58, 125, 21) then -- Camo
+			Neon.Color = Color3.fromRGB(99, 144, 59)
+		else
+			--print("")
+		end
+		if Neon.Color == Color3.fromRGB(52, 142, 64) then -- Sea green
+			Neon.Color = Color3.fromRGB(89, 172, 103)
+		else
+			--print("")
+		end
+		if Neon.Color == Color3.fromRGB(91, 154, 76) then -- Shamrock
+			Neon.Color = Color3.fromRGB(120, 157, 111)
+		else
+			--print("")
+		end
+		if Neon.Color == Color3.fromRGB(116, 134, 157) then -- Sand blue
+			Neon.Color = Color3.fromRGB(137, 147, 158)
+		else
+			--print("")
+		end
+		if Neon.Color == Color3.fromRGB(110, 153, 202) then -- Medium blue
+			Neon.Color = Color3.fromRGB(133, 161, 190)
+		else
+			--print("")
+		end
+		if Neon.Color == Color3.fromRGB(13, 105, 172) then -- Bright blue
+			Neon.Color = Color3.fromRGB(60, 129, 166)
+		else
+			--print("")
+		end
+		if Neon.Color == Color3.fromRGB(89, 34, 89) then -- Mulberry
+			Neon.Color = Color3.fromRGB(119, 75, 119)
+		else
+			--print("")
+		end
+		if Neon.Color == Color3.fromRGB(31, 128, 29) then -- Forest green
+			Neon.Color = Color3.fromRGB(78, 157, 74)
+		else
+			--print("")
+		end
+		if Neon.Color == Color3.fromRGB(75, 151, 75) then -- Bright green
+			Neon.Color = Color3.fromRGB(110, 156, 110)
+		else
+			--print("")
+		end
+		if Neon.Color == Color3.fromRGB(127, 142, 100) then -- Grime
+			Neon.Color = Color3.fromRGB(150, 155, 126)
+		else
+			--print("")
+		end
+		if Neon.Color == Color3.fromRGB(159, 243, 233) then -- Pastel blue-green
+			Neon.Color = Color3.fromRGB(146, 206, 198)
+		else
+			--print("")
+		end
+		if Neon.Color == Color3.fromRGB(159, 161, 172) then -- Fossil
+			Neon.Color = Color3.fromRGB(153, 154, 162)
+		else
+			--print("")
+		end
+		if Neon.Color == Color3.fromRGB(9, 137, 207) then -- Electric blue
+			Neon.Color = Color3.fromRGB(19, 155, 218)
+		else
+			--print("")
+		end
+		if Neon.Color == Color3.fromRGB(140, 91, 159) then -- Lavender
+			Neon.Color = Color3.fromRGB(152, 121, 152)
+		else
+			--print("")
+		end
+		if Neon.Color == Color3.fromRGB(98, 37, 209) then -- Royal purple
+			Neon.Color = Color3.fromRGB(125, 74, 161)
+		else
+			--print("")
+		end
+		if Neon.Color == Color3.fromRGB(123, 0, 123) then -- Eggplant
+			Neon.Color = Color3.fromRGB(142, 1, 142)
+		else
+			--print("")
+		end
+		if Neon.Color == Color3.fromRGB(120, 144, 130) then -- Sand green
+			Neon.Color = Color3.fromRGB(136, 150, 145)
+		else
+			--print("")
+		end
+		if Neon.Color == Color3.fromRGB(124, 156, 107) then -- Moss
+			Neon.Color = Color3.fromRGB(146, 170, 131)
+		else
+			--print("")
+		end
+		if Neon.Color == Color3.fromRGB(138, 171, 133) then -- Artichoke
+			Neon.Color = Color3.fromRGB(144, 160, 141)
+		else
+			--print("")
+		end
+		if Neon.Color == Color3.fromRGB(175, 221, 255) then -- Pastel light blue
+			Neon.Color = Color3.fromRGB(145, 174, 193)
+		else
+			--print("")
+		end
+		if Neon.Color == Color3.fromRGB(159, 173, 192) then -- Cadet blue
+			Neon.Color = Color3.fromRGB(164, 179, 198)
+		else
+			--print("")
+		end
+		if Neon.Color == Color3.fromRGB(4, 175, 236) then -- Cyan
+			Neon.Color = Color3.fromRGB(0, 162, 245)
+		else
+			--print("")
+		end
+		if Neon.Color == Color3.fromRGB(180, 128, 255) then -- Alder
+			Neon.Color = Color3.fromRGB(157, 142, 255)
+		else
+			--print("")
+		end
+		if Neon.Color == Color3.fromRGB(167, 94, 155) then -- Lilac
+			Neon.Color = Color3.fromRGB(170, 122, 170)
+		else
+			--print("")
+		end
+		if Neon.Color == Color3.fromRGB(123, 47, 123) then -- Plum
+			Neon.Color = Color3.fromRGB(152, 88, 163)
+		else
+			--print("")
+		end
+		if Neon.Color == Color3.fromRGB(107, 50, 124) then -- Bright violet
+			Neon.Color = Color3.fromRGB(131, 92, 144)
+		else
+			--print("")
+		end
+		if Neon.Color == Color3.fromRGB(193, 190, 66) then -- Olive
+			Neon.Color = Color3.fromRGB(177, 177, 104)
+		else
+			--print("")
+		end
+		if Neon.Color == Color3.fromRGB(164, 189, 71) then -- Br.yellowish green
+			Neon.Color = Color3.fromRGB(193, 250, 108)
+		else
+			--print("")
+		end
+		if Neon.Color == Color3.fromRGB(148, 190, 129) then -- Olivine
+			Neon.Color = Color3.fromRGB(148, 171, 136)
+		else
+			--print("")
+		end
+		if Neon.Color == Color3.fromRGB(168, 189, 153) then -- Laurel green
+			Neon.Color = Color3.fromRGB(174, 189, 164)
+		else
+			--print("")
+		end
+		if Neon.Color == Color3.fromRGB(128, 187, 219) then -- Pastel Blue
+			Neon.Color = Color3.fromRGB(146, 183, 203)
+		else
+			--print("")
+		end
+		if Neon.Color == Color3.fromRGB(177, 167, 255) then -- Pastel violet
+			Neon.Color = Color3.fromRGB(158, 151, 223)
+		else
+			--print("")
+		end
+		if Neon.Color == Color3.fromRGB(255, 102, 204) then -- Pink
+			Neon.Color = Color3.fromRGB(240, 127, 206)
+		else
+			--print("")
+		end
+		if Neon.Color == Color3.fromRGB(170, 0, 170) then -- Magenta
+			Neon.Color = Color3.fromRGB(159, 0, 177)
+		else
+			--print("")
+		end
+		if Neon.Color == Color3.fromRGB(151, 0, 0) then -- Crimson
+			Neon.Color = Color3.fromRGB(150, 2, 2)
+		else
+			--print("")
+		end
+		if Neon.Color == Color3.fromRGB(255, 176, 0) then -- Deep orange
+			Neon.Color = Color3.fromRGB(211, 142, 4)
+		else
+			--print("")
+		end
+		if Neon.Color == Color3.fromRGB(161, 196, 140) then -- Medium green
+			Neon.Color = Color3.fromRGB(151, 170, 139)
+		else
+			--print("")
+		end
+		if Neon.Color == Color3.fromRGB(177, 229, 166) then -- Mint
+			Neon.Color = Color3.fromRGB(170, 203, 162)
+		else
+			--print("")
+		end
+		if Neon.Color == Color3.fromRGB(204, 255, 204) then -- Pastel green
+			Neon.Color = Color3.fromRGB(175, 218, 175)
+		else
+			--print("")
+		end
+		if Neon.Color == Color3.fromRGB(180, 210, 228) then -- Light blue
+			Neon.Color = Color3.fromRGB(168, 196, 212)
+		else
+			--print("")
+		end
+		if Neon.Color == Color3.fromRGB(152, 194, 219) then -- Baby blue
+			Neon.Color = Color3.fromRGB(148, 179, 195)
+		else
+			--print("")
+		end
+		if Neon.Color == Color3.fromRGB(255, 89, 89) then -- Persimmon
+			Neon.Color = Color3.fromRGB(190, 119, 119)
+		else
+			--print("")
+		end
+		if Neon.Color == Color3.fromRGB(196, 40, 28) then -- Bright red
+			Neon.Color = Color3.fromRGB(224, 82, 69)
+		else
+			--print("")
+		end
+		if Neon.Color == Color3.fromRGB(117, 0, 0) then -- Maroon
+			Neon.Color = Color3.fromRGB(137, 4, 4)
+		else
+			--print("")
+		end
+		if Neon.Color == Color3.fromRGB(239, 184, 56) then -- Gold
+			Neon.Color = Color3.fromRGB(212, 175, 95)
+		else
+			--print("")
+		end
+		if Neon.Color == Color3.fromRGB(245, 205, 48) then -- Bright yellow
+			Neon.Color = Color3.fromRGB(199, 176, 90)
+		else
+			--print("")
+		end
+		if Neon.Color == Color3.fromRGB(248, 217, 109) then -- Daisy orange
+			Neon.Color = Color3.fromRGB(230, 194, 131)
+		else
+			--print("")
+		end
+		if Neon.Color == Color3.fromRGB(255, 255, 204) then -- Pastel yellow
+			Neon.Color = Color3.fromRGB(255, 255, 171)
+		else
+			--print("")
+		end
+		if Neon.Color == Color3.fromRGB(199, 212, 228) then -- Fog
+			Neon.Color = Color3.fromRGB(175, 201, 237)
+		else
+			--print("")
+		end
+		if Neon.Color == Color3.fromRGB(224, 178, 208) then -- Mauve
+			Neon.Color = Color3.fromRGB(200, 169, 190)
+		else
+			--print("")
+		end
+		if Neon.Color == Color3.fromRGB(212, 144, 189) then -- Sunrise
+			Neon.Color = Color3.fromRGB(186, 154, 177)
+		else
+			--print("")
+		end
+		if Neon.Color == Color3.fromRGB(190, 104, 98) then -- Terra Cotta
+			Neon.Color = Color3.fromRGB(218, 129, 126)
+		else
+			--print("")
+		end
+		if Neon.Color == Color3.fromRGB(163, 75, 75) then -- Dusty Rose
+			Neon.Color = Color3.fromRGB(166, 110, 110)
+		else
+			--print("")
+		end
+		if Neon.Color == Color3.fromRGB(86, 36, 36) then -- Cocoa
+			Neon.Color = Color3.fromRGB(116, 77, 77)
+		else
+			--print("")
+		end
+		if Neon.Color == Color3.fromRGB(213, 115, 61) then -- Neon orange
+			Neon.Color = Color3.fromRGB(205, 137, 100)
+		else
+			--print("")
+		end
+		if Neon.Color == Color3.fromRGB(218, 133, 65) then -- Bright orange
+			Neon.Color = Color3.fromRGB(226, 156, 102)
+		else
+			--print("")
+		end
+		if Neon.Color == Color3.fromRGB(241, 231, 199) then -- Wheat
+			Neon.Color = Color3.fromRGB(212, 203, 176)
+		else
+			--print("")
+		end
+		if Neon.Color == Color3.fromRGB(254, 243, 187) then -- Buttermilk
+			Neon.Color = Color3.fromRGB(226, 216, 167)
+		else
+			--print("")
+		end
+		if Neon.Color == Color3.fromRGB(232, 186, 200) then -- Light reddish violet
+			Neon.Color = Color3.fromRGB(203, 163, 176)
+		else
+			--print("")
+		end
+		if Neon.Color == Color3.fromRGB(255, 201, 201) then -- Pastel orange
+			Neon.Color = Color3.fromRGB(202, 166, 166)
+		else
+			--print("")
+		end
+		if Neon.Color == Color3.fromRGB(255, 148, 148) then -- Salmon
+			Neon.Color = Color3.fromRGB(235, 146, 146)
+		else
+			--print("")
+		end
+		if Neon.Color == Color3.fromRGB(150, 85, 85) then -- Tawny
+			Neon.Color = Color3.fromRGB(155, 118, 118)
+		else
+			--print("")
+		end
+		if Neon.Color == Color3.fromRGB(143, 76, 42) then -- Rust
+			Neon.Color = Color3.fromRGB(162, 111, 88)
+		else
+			--print("")
+		end
+		if Neon.Color == Color3.fromRGB(170, 85, 0) then -- CGA brown
+			Neon.Color = Color3.fromRGB(158, 117, 3)
+		else
+			--print("")
+		end
+		if Neon.Color == Color3.fromRGB(226, 155, 64) then -- Br.yellowish orange
+			Neon.Color = Color3.fromRGB(194, 151, 102)
+		else
+			--print("")
+		end
+		if Neon.Color == Color3.fromRGB(211, 190, 150) then -- Cashmere
+			Neon.Color = Color3.fromRGB(198, 181, 151)
+		else
+			--print("")
+		end
+		if Neon.Color == Color3.fromRGB(233, 218, 218) then -- Seashell
+			Neon.Color = Color3.fromRGB(217, 186, 186)
+		else
+			--print("")
+		end
+		if Neon.Color == Color3.fromRGB(255, 204, 153) then -- Pastel brown
+			Neon.Color = Color3.fromRGB(185, 162, 139)
+		else
+			--print("")
+		end
+		if Neon.Color == Color3.fromRGB(234, 184, 146) then -- Light orange
+			Neon.Color = Color3.fromRGB(224, 170, 140)
+		else
+			--print("")
+		end
+		if Neon.Color == Color3.fromRGB(218, 134, 122) then -- Medium red
+			Neon.Color = Color3.fromRGB(238, 152, 141)
+		else
+			--print("")
+		end
+		if Neon.Color == Color3.fromRGB(136, 62, 62) then -- Burgundy
+			Neon.Color = Color3.fromRGB(159, 101, 101)
+		else
+			--print("")
+		end
+		if Neon.Color == Color3.fromRGB(105, 64, 40) then -- Reddish brown
+			Neon.Color = Color3.fromRGB(129, 102, 83)
+		else
+			--print("")
+		end
+		if Neon.Color == Color3.fromRGB(188, 155, 93) then -- Cork
+			Neon.Color = Color3.fromRGB(198, 170, 122)
+		else
+			--print("")
+		end
+		if Neon.Color == Color3.fromRGB(199, 172, 120) then -- Burlap
+			Neon.Color = Color3.fromRGB(199, 177, 135)
+		else
+			--print("")
+		end
+		if Neon.Color == Color3.fromRGB(202, 191, 163) then -- Beige
+			Neon.Color = Color3.fromRGB(218, 206, 176)
+		else
+			--print("")
+		end
+		if Neon.Color == Color3.fromRGB(215, 197, 154) then -- Brick yellow
+			Neon.Color = Color3.fromRGB(207, 193, 161)
+		else
+			--print("")
+		end
+		if Neon.Color == Color3.fromRGB(204, 142, 105) then -- Nougat
+			Neon.Color = Color3.fromRGB(206, 156, 129)
+		else
+			--print("")
+		end
+		if Neon.Color == Color3.fromRGB(124, 92, 70) then -- Brown
+			Neon.Color = Color3.fromRGB(144, 121, 107)
+		else
+			--print("")
+		end
+		if Neon.Color == Color3.fromRGB(108, 88, 75) then -- Pine Cone
+			Neon.Color = Color3.fromRGB(129, 117, 110)
+		else
+			--print("")
+		end
+		if Neon.Color == Color3.fromRGB(160, 132, 79) then -- Fawn brown
+			Neon.Color = Color3.fromRGB(177, 155, 113)
+		else
+			--print("")
+		end
+		if Neon.Color == Color3.fromRGB(149, 121, 119) then -- Sand red
+			Neon.Color = Color3.fromRGB(153, 137, 137)
+		else
+			--print("")
+		end
+		if Neon.Color == Color3.fromRGB(149, 137, 136) then -- Hurrican grey
+			Neon.Color = Color3.fromRGB(170, 156, 155)
+		else
+			--print("")
+		end
+		if Neon.Color == Color3.fromRGB(175, 148, 131) then -- Linen
+			Neon.Color = Color3.fromRGB(176, 159, 149)
+		else
+			--print("")
+		end
+		if Neon.Color == Color3.fromRGB(150, 103, 102) then -- Copper
+			Neon.Color = Color3.fromRGB(151, 127, 127)
+		else
+			--print("")
+		end
+		if Neon.Color == Color3.fromRGB(160, 95, 53) then -- Dark orange
+			Neon.Color = Color3.fromRGB(160, 123, 93)
+		else
+			--print("")
+		end
+		if Neon.Color == Color3.fromRGB(86, 66, 54) then -- Dirt brown
+			Neon.Color = Color3.fromRGB(116, 103, 94)
+		else
+			--print("")
+		end
+		if Neon.Color == Color3.fromRGB(126, 104, 63) then -- Bronze
+			Neon.Color = Color3.fromRGB(143, 128, 103)
+		else
+			--print("")
+		end
+		if Neon.Color == Color3.fromRGB(99, 95, 98) then -- Dark stone grey
+			Neon.Color = Color3.fromRGB(126, 123, 125)
+		else
+			--print("")
+		end
+		if Neon.Color == Color3.fromRGB(105, 102, 92) then -- Flint
+			Neon.Color = Color3.fromRGB(128, 126, 121)
+		else
+			--print("")
+		end
+		if Neon.Color == Color3.fromRGB(90, 76, 66) then -- Dark taupe
+			Neon.Color = Color3.fromRGB(119, 110, 104)
+		else
+			--print("")
+		end
+		if Neon.Color == Color3.fromRGB(106, 57, 9) then -- Burnt Sienna
+			Neon.Color = Color3.fromRGB(128, 97, 59)
+		else
+			--print("")
+		end
+		if Neon.Color == Color3.fromRGB(17, 17, 17) then -- Really black
+			Neon.Color = Color3.fromRGB(54, 54, 54)
+		else
+			--print("")
+		end
+		if Neon:IsA("UnionOperation") then
+			if Neon.UsePartColor == false then
+				Neon.UsePartColor = true
+			else
+				-- print("")
+			end
+		end
+	else
+		--print("")
+	end
+end
+
+function getnewpart(part)
+	if part:IsA("Part") or part:IsA("WedgePart") or part:IsA("CornerWedgePart") or part:IsA("TrussPart") or part:IsA("BasePart") or part:IsA("MeshPart") then
+		changecolors(part)
+		part:GetPropertyChangedSignal("Color"):Connect(function()
+			changecolors(part)
+		end)
+		part:GetPropertyChangedSignal("Material"):Connect(function()
+			changecolors(part)
+		end)
+	end
+end
+
+game.workspace.DescendantAdded:Connect(function(part)
+	for i,v in pairs(game.workspace:GetDescendants()) do
+		getnewpart(v)
+	end
+end)
